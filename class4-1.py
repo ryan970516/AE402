543ea22435adc364216955e83976409c7d94f0e3
ryan970516 <102479@stu.japs.tp.edu.tw>
2020-03-28 15:23:09 +0800

    class4

---
+++ class4-1.py
@@ -0,0 +1,70 @@
+"""
+ Pygame 模板程式
+ 
+"""
+# 匯入pygame模組
+import pygame
+import random
+
+def randomcolor():
+    r = random.randrange(255)
+    g = random.randrange(255)
+    b = random.randrange(255)
+    return (r,g,b)
+    
+# 定義一些會用到的顏色
+# 常數使用大寫
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+BLUE     = (   0,   0, 255)
+
+# 初始化pygame
+pygame.init()
+
+# 創造一個pygame視窗並設定大小及標題
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("我的遊戲")
+
+# 設定一個開關供迴圈使用
+done = False
+
+# 創造一個clock控制畫面更新速度
+clock = pygame.time.Clock()
+x = 0
+y = 0
+circlesize = 100
+color = (0,0,0)
+# -------- 主要的程式迴圈 -----------
+while not done:
+    # --- 事件迴圈 event loop
+    for event in pygame.event.get(): # 從事件list中抓取事件
+        if event.type == pygame.QUIT: # 當使用者按下結束
+            done = True # 將done變數設為True-->while迴圈將會結束
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            
+            x,y = pygame.mouse.get_pos()
+            circlesize = 0
+            color = randomcolor()
+    # --- 程式的運算與邏輯
+    
+
+    # --- 繪圖的程式碼
+    #       先將畫面塗滿底色(將原有畫面清掉)
+    #       繪圖的程式要寫在這行後面，不然會被這行清掉
+    screen.fill(BLACK)
+    if circlesize < 100:
+        circlesize += 1
+        pygame.draw.circle(screen, color, (x,y),circlesize)
+    # --- 更新畫面
+    pygame.display.flip()
+
+    # --- 每秒鐘60個frame
+    clock.tick(500)
+
+# 關閉式窗並離開程式
+pygame.quit()
+exit()
